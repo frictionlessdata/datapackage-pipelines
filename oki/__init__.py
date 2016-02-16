4a31d340f6a63bccd34dac2febfece1ad40be4cc
Paul Walsh <paulywalsh@gmail.com>
2016-02-16 19:48:14 +0200

    Add our codebase, and a VERSION file for the version number.

---
+++ oki/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import io
+import os
+from .main import what_we_want
+
+VERSION_FILE = os.path.join(os.path.dirname(__file__), 'VERSION')
+
+__version__ = io.open(VERSION_FILE, encoding='utf-8').readline().strip()
+
+__all__ = ['what_we_want']
---
+++ oki/main.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+
+def what_we_want():
+    '''Returns what we want for open data.'''
+    return 'We want the data raw, and we want the data now.'
