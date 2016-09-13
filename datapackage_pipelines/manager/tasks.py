76abd1fa5af09e5faaf874d61df74ba54348b835
Adam Kariv <adam.kariv@okfn.org>
2016-09-13 10:49:17 +0300

    Use asyncio module instead of subprocess for better performance

--- datapackage_pipelines/manager/tasks.py
+++ datapackage_pipelines/manager/tasks.py
@@ -1,11 +1,9 @@
+import os
 import sys
 import json
 import logging
-import subprocess
-from threading import Thread
-from queue import Queue, Empty
 
-import shutil
+import asyncio
 from celery import current_app
 from .status import status
 
@@ -14,91 +12,105 @@ logging.basicConfig(level=logging.DEBUG,
                     format="%(levelname)-8s:"+runner+":%(message)s")
 
 
-def enqueue_output(out, queue):
-    for line in iter(out.readline, b''):
-        line = line.rstrip()
-        if line != '':
+async def enqueue_errors(process, queue):
+    out = process.stderr
+    while True:
+        line = await out.readline()
+        if line == b'':
+            break
+        line = line.decode('utf8').rstrip()
+        if len(line) != 0:
             print(line)
-            queue.put(line)
-    out.close()
+            await queue.put(line)
 
 
-def execute_pipeline(pipeline_id, pipeline_steps, trigger='manual'):
+async def dequeue_errors(queue, out):
+    while True:
+        line = await queue.get()
+        if line is None:
+            break
+        out.append(line)
+        if len(out) > 1000:
+            out.pop(0)
+
+
+def create_process(args, wfd, rfd):
+    pass_fds = {rfd, wfd}
+    if None in pass_fds:
+        pass_fds.remove(None)
+    rfd = asyncio.subprocess.PIPE if rfd is None else rfd
+    wfd = asyncio.subprocess.DEVNULL if wfd is None else wfd
+    ret = asyncio.create_subprocess_exec(*args,
+                                         stdin=rfd,
+                                         stdout=wfd,
+                                         stderr=asyncio.subprocess.PIPE,
+                                         pass_fds=pass_fds)
+    return ret
+
+async def process_death_waiter(process):
+    return_code = await process.wait()
+    return process, return_code
+
+async def async_execute_pipeline(pipeline_id, pipeline_steps, trigger='manual'):
 
     status.running(pipeline_id, trigger, '')
 
-    stdout = None
-    stderr = Queue()
+    rfd = None
+    error_queue = asyncio.Queue()
+    errors = []
+    error_aggregator = asyncio.ensure_future(dequeue_errors(error_queue, errors))
+    error_collectors = []
 
     processes = []
     logging.info("RUNNING %s:", pipeline_id)
     for i, step in enumerate(pipeline_steps):
+
+        if i != len(pipeline_steps)-1:
+            new_rfd, wfd = os.pipe()
+        else:
+            new_rfd, wfd = None, None
+
         logging.info("- %s", step['run'])
         args = [
             sys.executable,
             step['run'],
             str(i),
-            json.dumps(step.get('parameters', {}))
+            json.dumps(step.get('parameters', {})),
+            str(step.get('validate', False))
         ]
-        process = subprocess.Popen(args,
-                                   stdin=stdout,
-                                   stdout=subprocess.PIPE,
-                                   stderr=subprocess.PIPE,
-                                   universal_newlines=True)
+        process = await create_process(args, wfd, rfd)
+        process.args = args[1]
+        if wfd is not None:
+            os.close(wfd)
+        if rfd is not None:
+            os.close(rfd)
+
         processes.append(process)
-        stdout = process.stdout
-        t = Thread(target=enqueue_output, args=(process.stderr, stderr))
-        t.daemon = True # thread dies with the program
-        t.start()
+        rfd = new_rfd
+        error_collectors.append(asyncio.ensure_future(enqueue_errors(process, error_queue)))
 
-    if stdout is not None:
-        shutil.copyfileobj(stdout, open(pipeline_id, 'w'))
-
-    errors = []
-    return_codes = []
-    kill = False
-    rounds = 0
-    while len(processes) > 0:
-        rounds += 1
-        if rounds > 3600 and not kill:
-            logging.error("TIMED OUT %s:%s", pipeline_id, process.args[1])
-            kill = True
-
-        ret_code = None
-        for process in processes:
-            if kill:
-                logging.error("KILLING %s:%s", pipeline_id, process.args[1])
-                process.kill()
-                processes.remove(process)
-                ret_code = 'KILLED'
-                break
+    pending = [asyncio.ensure_future(process_death_waiter(process)) for process in processes]
+    while len(pending) > 0:
+        done, pending = await asyncio.wait(pending, return_when=asyncio.FIRST_COMPLETED)
+        for waiter in done:
+            process, return_code = waiter.result()
+            if return_code == 0:
+                logging.info("DONE %s", process.args)
+                processes = [p for p in processes if p.pid != process.pid]
             else:
-                # logging.info("WAITING FOR %s:%s", pipeline_id, process.args[1])
-                try:
-                    ret_code = process.wait(timeout=1)
-                    kill = kill or ret_code != 0
-                    processes.remove(process)
-                    break
-                except subprocess.TimeoutExpired:
-                    pass
+                logging.error("FAILED %s: %s", process.args, return_code)
+                for to_kill in processes:
+                    to_kill.kill()
 
-        while not stderr.empty():
-            errors.append(stderr.get())
-            if len(errors) > 1000:
-                errors.pop(0)
+    await asyncio.gather(*error_collectors)
+    await error_queue.put(None)
+    await error_aggregator
 
-        status.running(pipeline_id, trigger, '\n'.join(errors))
 
-        if ret_code is not None:
-            return_codes.append(ret_code)
-
-    logging.info("DONE %s: %s", pipeline_id, return_codes)
-
-    status.idle(pipeline_id,
-                return_codes[-1] == 0,
-                '\n'.join(errors))
-
-    assert return_codes[-1] == 0
+def execute_pipeline(pipeline_id, pipeline_steps):
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(async_execute_pipeline(pipeline_id, pipeline_steps))
+    loop.close()
 
 
 @current_app.task
