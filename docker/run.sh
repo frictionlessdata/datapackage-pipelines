001bd5ed0aa4acc016dced5e45dc0d117dd2d633
Adam Kariv <adam.kariv@okfn.org>
2017-04-02 10:16:38 +0300

    Wait for redis server to start before doing anything else

--- docker/run.sh
+++ docker/run.sh
@@ -3,6 +3,7 @@ if [ "$1" = "server" ]; then
     export DPP_REDIS_HOST=127.0.0.1
     echo "Starting Server"
     redis-server /etc/redis.conf --appendonly yes --daemonize yes --dir /var/redis
+    until [ `redis-cli ping | grep -c PONG` = 1 ]; do echo "Waiting 1s for Redis to load"; sleep 1; done
     rm -f celeryd.pid
     python3 -m celery --detach -b redis://localhost:6379/0 --concurrency=4 -B -A datapackage_pipelines.app -Q datapackage-pipelines -l INFO worker
     dpp serve
