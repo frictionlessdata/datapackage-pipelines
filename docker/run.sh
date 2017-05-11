29b1d4d52f7fbb0179ec09ac11645e818e466c5b
Adam Kariv <adam.kariv@okfn.org>
2017-05-11 05:30:12 +0300

    Don't redis append only

--- docker/run.sh
+++ docker/run.sh
@@ -2,7 +2,7 @@
 if [ "$1" = "server" ]; then 
     export DPP_REDIS_HOST=127.0.0.1
     echo "Starting Server"
-    redis-server /etc/redis.conf --appendonly yes --daemonize yes --dir /var/redis
+    redis-server /etc/redis.conf --daemonize yes --dir /var/redis
     until [ `redis-cli ping | grep -c PONG` = 1 ]; do echo "Waiting 1s for Redis to load"; sleep 1; done
     rm -f celeryd.pid
     rm -f celerybeat.pid
