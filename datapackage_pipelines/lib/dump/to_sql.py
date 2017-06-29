9005968e865893f43f8a34cff07b6b5752f3e8b3
Adam Kariv <adam.kariv@okfn.org>
2017-06-29 21:28:44 +0300

    Support loading multiple resources in load_resource

--- datapackage_pipelines/lib/dump/to_sql.py
+++ datapackage_pipelines/lib/dump/to_sql.py
@@ -3,6 +3,7 @@ import logging
 from functools import partial
 from jsontableschema_sql import Storage
 from sqlalchemy import create_engine
+from sqlalchemy.exc import OperationalError
 
 from datapackage_pipelines.lib.dump.dumper_base import DumperBase
 
