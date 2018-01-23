86595bac556b321646a4d8b80479ee1c524f5f51
Adam Kariv <adam.kariv@okfn.org>
2018-01-23 18:05:44 +0200

    1.5.4: support compressed sources in stream_remote_resources, dependencies in load_metadata

--- datapackage_pipelines/lib/load_metadata.py
+++ datapackage_pipelines/lib/load_metadata.py
@@ -1,10 +1,16 @@
 import datapackage
 
-from datapackage_pipelines.wrapper import ingest, spew
+from datapackage_pipelines.wrapper import ingest, spew, get_dependency_datapackage_url
+
+dep_prefix = 'dependency://'
 
 parameters, dp, res_iter = ingest()
 
 url = parameters['url']
+if url.startswith(dep_prefix):
+    dependency = url[len(dep_prefix):].strip()
+    url = get_dependency_datapackage_url(dependency)
+    assert url is not None, "Failed to fetch output datapackage for dependency '%s'" % dependency
 
 datapackage = datapackage.DataPackage(url)
 for k, v in datapackage.descriptor.items():
