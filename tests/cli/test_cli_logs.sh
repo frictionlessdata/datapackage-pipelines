6e0692049730700548797ab711cef540d0f7b8d5
Ori Hoch <ori@uumpa.com>
2018-03-15 11:27:06 +0200

    disable pretty progress when running with --verbose

--- tests/cli/test_cli_logs.sh
+++ tests/cli/test_cli_logs.sh
@@ -2,13 +2,13 @@
 
 TEMPDIR=`mktemp -d`
 
-! script -ec "dpp run --verbose ./tests/cli/verbose-logs-with-sleep" $TEMPDIR/log && echo failed to run && exit 1
+! script -ec "dpp run --verbose ./tests/cli/verbose-logs-with-sleep" $TEMPDIR/verbose_log && echo failed to run with --verbose && exit 1
+cat -v $TEMPDIR/verbose_log | grep '\^\[\[[0-9][0-9]*A' && echo running with --verbose - found terminal escape sequences && exit 1
 
-# dump the non-escaped sequences to visualize the problem
-# TODO: figure out how to test for missing log line due to terminal escape sequences
-cat -v $TEMPDIR/log
+! script -ec "dpp run ./tests/cli/verbose-logs-with-sleep" $TEMPDIR/log && echo failed to run without verbose && exit 1
+! cat -v $TEMPDIR/log | grep '\^\[\[[0-9][0-9]*A' && echo running without verbose - did not find terminal escape sequences && exit 1
 
 rm -rf "${TEMPDIR}"
 
-echo Great Success?
+echo Great Success
 exit 0
