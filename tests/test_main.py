5917d23466e93229390c60fd422cb99d90a8ab02
Ori Hoch <ori@uumpa.com>
2018-08-08 14:26:10 +0300

    added test for pipeline with code step (#123)
    * add test for code step
    
    * add run attribute to code test

--- tests/test_main.py
+++ tests/test_main.py
@@ -44,7 +44,9 @@ def test_pipeline():
                             force=False,
                             concurrency=1,
                             verbose_logs=True)
-    assert all(result.success for result in results)
+    failed_results = list(filter(lambda r: not r.success, results))
+    assert len(failed_results) == 0, "Failed results: {}".format(["{} {}".format(result.pipeline_id, ", ".join(result.errors))
+                                                                 for result in failed_results])
     assert len(called_hooks) == 3
     assert called_hooks == [
         {"pipeline_id": "./tests/env/dummy/pipeline-test-hooks", "event": "queue"},
