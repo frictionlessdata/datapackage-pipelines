7bc4481033af68e8d40d3c3f9f76adb4cea6e6ae
Adam Kariv <adam.kariv@okfn.org>
2018-10-16 15:47:44 +0300

    Save datapackage location in stats for dump_to_path

--- datapackage_pipelines/lib/dump_to_path.py
+++ datapackage_pipelines/lib/dump_to_path.py
@@ -1,10 +1,15 @@
+import os
+
 from dataflows import Flow, dump_to_path
 from datapackage_pipelines.wrapper import ingest
 from datapackage_pipelines.utilities.flow_utils import spew_flow
 
+from datapackage_pipelines.utilities.stat_utils import STATS_DPP_KEY, STATS_OUT_DP_URL_KEY
 
-def flow(parameters: dict):
+
+def flow(parameters: dict, stats: dict):
     out_path = parameters.pop('out-path', '.')
+    stats.setdefault(STATS_DPP_KEY, {})[STATS_OUT_DP_URL_KEY] = os.path.join(out_path, 'datapackage.json')
     return Flow(
         dump_to_path(
             out_path,
@@ -15,4 +20,4 @@ def flow(parameters: dict):
 
 if __name__ == '__main__':
     with ingest() as ctx:
-        spew_flow(flow(ctx.parameters), ctx)
+        spew_flow(flow(ctx.parameters, ctx.stats), ctx)
