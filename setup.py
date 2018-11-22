57c78bc4a9d320146e1fd38a94d6757b21292f86
Adam Kariv <adam.kariv@okfn.org>
2018-11-22 13:54:15 +0200

    Dump dataflows dep

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ INSTALL_REQUIRES = [
     'cachetools',
     'tabulator>=1.17.0',
     'globster>=0.1.0',
-    'dataflows>=0.0.29',
+    'dataflows>=0.0.34',
 ]
 SPEEDUP_REQUIRES = [
     'dataflows[speedup]',
