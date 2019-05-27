aa0e35c530e9a20889985c7c8017d69ef626006f
Adam Kariv <adam.kariv@okfn.org>
2019-05-27 10:28:39 +0300

    tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -53,7 +53,7 @@ def test_pipeline():
         {"pipeline_id": "./tests/env/dummy/pipeline-test-hooks", "event": "start"},
         {"pipeline_id": "./tests/env/dummy/pipeline-test-hooks", "event": "finish", "success": True,
          'stats': {'.dpp': {'out-datapackage-url': 'hooks-outputs/datapackage.json'},
-                   'bytes': 15746, 'count_of_rows': 40,
+                   'bytes': 15715, 'count_of_rows': 40,
                    'dataset_name': 'hook-tests', 'hash': 'aa1687f3c157e5bd733d102de0e80d29'}}
     ]
     assert progresses >= 1
