3b94aad35e09b5acd29eaa53ee0acb30f5ad9945
Adam Kariv <adam.kariv@okfn.org>
2019-10-21 16:18:34 +0300

    v2.1.12 Fix flow hash calculation method

--- datapackage_pipelines/specs/resolver.py
+++ datapackage_pipelines/specs/resolver.py
@@ -83,9 +83,9 @@ def resolve_executor(step, path, errors):
         flow = step.pop('flow')
         parameters.update(__flow=flow, __path=path)
         try:
-            flow_py_file = os.path.join(path, flow.replace('.', '/') + '.py')
-            if os.path.exists(flow_py_file):
-                parameters['__flow_hash'] = hashlib.md5(open(flow_py_file, 'rb').read()).hexdigest()
+            m = import_module(flow)
+            flow_py_file = m.__file__
+            parameters['__flow_hash'] = hashlib.md5(open(flow_py_file, 'rb').read()).hexdigest()
         except Exception:
             pass
 
